d0debf1323492028294a11db84a64e10037ad511
Mario Pohrib <m9@Mario-MacBook-Air.local>
2026-04-04 21:56:04 +0200

    Fix

---
+++ actions/check_files/index.js
@@ -0,0 +1,13 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput("who-to-say-goodbye");
+    core.info(`GoodBye ${nameToGreet}`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+
+} catch (error) {
+  	core.setFailed(error.message);
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,29 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-const config = {
-  input: "src/index.js",
-  output: {
-    esModule: true,
-    file: "dist/index.js",
-    format: "es",
-    sourcemap: true,
+const config = [{
+
+    input: "src/index.js",
+    output: {
+      esModule: true,
+      file: "dist/index.js",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
   },
-  plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
-};
+
+  {
+    input: "actions/check_files/index.js",
+    output: {
+      esModule: true, 
+      file: "actions/check_files/dist/index.js",
+      format: "es" ,
+      sourcemap: true,
+    },
+    plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
+  }  
+]
 
 export default config;
 
